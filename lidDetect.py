9737d72ac59877d44dc3a627a3d4d00873bd09a7
Darc-Charlie <charlesisichei@gmail.com>
2017-03-06 17:57:34 -0600

    add try and except block

--- lidDetect.py
+++ lidDetect.py
@@ -21,26 +21,30 @@ cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT, 1,50,
                              param1=50, param2=30, minRadius=400, maxRadius=450)
 
-circles = np.uint16(np.around(circles))
-print circles   
-for i in circles[0,:]:
-    # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+try:
+    circles = np.uint16(np.around(circles))
+    print circles   
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-cv2.circle(cimg,(xCntr,yCntr),2,(181,38,213),3)
-distance = round(math.sqrt((pow(circles[0][0][0] - xCntr,2)) + (pow(circles[0][0][1] - yCntr,2))))
-print distance
-port.write(struct.pack('>B', distance))
+    cv2.circle(cimg,(xCntr,yCntr),2,(181,38,213),3)
+    distance = round(math.sqrt((pow(circles[0][0][0] - xCntr,2)) + (pow(circles[0][0][1] - yCntr,2))))
+    print distance
+    port.write(struct.pack('>B', distance))
 
-for j in circles[0,:]:
-    if j[0]-xCntr < 0:
-        print 'left'
-    else:
-        print 'right'
-    if j[1]-yCntr < 0:
-        print 'up'
-    else:
-        print 'down'
+    for j in circles[0,:]:
+        if j[0]-xCntr < 0:
+            print 'left'
+        else:
+            print 'right'
+        if j[1]-yCntr < 0:
+            print 'up'
+        else:
+            print 'down'
+  
+except AttributeError:
+    print "No cirles detected"
         
